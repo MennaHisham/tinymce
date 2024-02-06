d20cb29aadd44eda279cf4a5f9efa6f7ef13d22e
Minatallah Hisham <mennahishamh@gmail.com>
2024-02-06 16:35:52 +0200

    create rich editor

---
+++ my-app/src/app/_components/RichEditor.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import BundledEditor from './tinymce/BundledEditor';
+
+const RichEditor = () => {
+  return (
+    <>
+      <BundledEditor />
+    </>
+  );
+};
